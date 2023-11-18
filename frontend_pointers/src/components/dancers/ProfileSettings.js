6386f31ee7943c9bd1a76c1ca593b4f70bce2b6d
Deen037 <deen.bds@gmail.com>
2023-11-18 21:44:31 +0100

    refactor settings form

--- frontend_pointers/src/components/dancers/ProfileSettings.js
+++ frontend_pointers/src/components/dancers/ProfileSettings.js
@@ -1,9 +1,38 @@
-import "./../../styles/ProfileSettings.css";
 import React from "react";
 import {useSelector} from "react-redux";
+import "./../../styles/ProfileSettings.css";
 
 function ProfileSettings() {
     const dancer = useSelector(state => state.user.currentUser);
+
+    const formFields = [
+        {label: 'Username', value: dancer.username},
+        {label: 'First Name', value: dancer.firstName},
+        {label: 'Last Name', value: dancer.lastName},
+        {label: 'Birthdate', value: dancer.birthdate, type: "date"},
+        {label: 'Phone Number', value: dancer.phone},
+        {label: 'City', value: dancer.city},
+        {label: 'Country', value: dancer.country},
+        {label: 'Nationality', value: dancer.nationality},
+        {label: 'Photo', value: dancer.photo},
+        {label: 'About', value: dancer.about},
+        {label: 'Instagram', value: dancer.instagram},
+        {label: 'Facebook', value: dancer.facebook},
+        {label: 'Youtube', value: dancer.youtube},
+        {label: 'Twitter', value: dancer.twitter},
+        {label: 'Tiktok', value: dancer.tiktok},
+        {label: 'Spotify', value: dancer.spotify},
+        {label: 'Soundcloud', value: dancer.soundcloud},
+        {label: 'Twitch', value: dancer.twitch},
+    ];
+
+    const renderFormField = (field, index) => (
+        <div className={`row`} key={index}>
+            <label>{field.label}: </label>
+            <input value={field.value} type={field.type}/>
+        </div>
+    );
+
     return (
         <div className="profile-container">
             <div className="profile-header">
@@ -12,103 +41,29 @@ function ProfileSettings() {
             <div className={`settings-forms`}>
                 <div className={`settings-form-left`}>
                     <form>
-                        <div className={`row`}>
-                            <label>Username: </label>
-                            <input placeholder={dancer.username}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>First Name: </label>
-                            <input placeholder={dancer.firstName}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Last Name: </label>
-                            <input placeholder={dancer.lastName}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Birthdate: </label>
-                            <input placeholder={dancer.birthdate} type="date"/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Phone Number: </label>
-                            <input placeholder={dancer.phone}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>City :</label>
-                            <input placeholder={dancer.city}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Country :</label>
-                            <input placeholder={dancer.country}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Nationality :</label>
-                            <input placeholder={dancer.nationality}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Photo</label>
-                            <input placeholder={dancer.photo}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>About: </label>
-                            <input placeholder={dancer.about}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Instagram: </label>
-                            <input placeholder={dancer.instagram}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Facebook: </label>
-                            <input placeholder={dancer.facebook}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Youtube: </label>
-                            <input placeholder={dancer.youtube}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Twitter: </label>
-                            <input placeholder={dancer.twitter}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Tiktok: </label>
-                            <input placeholder={dancer.tiktok}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Spotify: </label>
-                            <input placeholder={dancer.spotify}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Soundcloud: </label>
-                            <input placeholder={dancer.soundcloud}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Twitch: </label>
-                            <input placeholder={dancer.twitch}/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Change Password: </label>
-                            <input placeholder="New Password" type="password"/>
-                        </div>
-                        <div className={`row`}>
-                            <label>Confirm Password: </label>
-                            <input placeholder="Confirm Password" type="password"/>
-                        </div>
+                        {formFields.map(renderFormField)}
                     </form>
                 </div>
-                <div className={`settings-form-right`}>
-                    <div className={`row`}>
-                        <input className={`button`} type="button" value="Set Crew"/>
-                    </div>
-                    <div className={`row`}>
-                        <input className={`button`} type="button" value="Set Event"/>
-                    </div>
-                </div>
+                <SettingsFormRight/>
             </div>
             <div className={`done`}>
                 <input className={`button`} type="button" value="Done"/>
             </div>
-
         </div>
-    )
+    );
+}
+
+function SettingsFormRight() {
+    return (
+        <div className={`settings-form-right`}>
+            <div className={`row`}>
+                <input className={`button`} type="button" value="Set Crew"/>
+            </div>
+            <div className={`row`}>
+                <input className={`button`} type="button" value="Set Event"/>
+            </div>
+        </div>
+    );
 }
 
 export default ProfileSettings;
